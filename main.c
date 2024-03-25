487549f145ef52e58850cea1ab8f6cbf7711e3b7
Stefan Birsan <stefbir10@gmail.com>
2024-03-25 22:24:02 +0200

    game logic v.1

--- main.c
+++ main.c
@@ -18,6 +18,98 @@
 #define GRID_SIZE 20
 #define GRID_DIM 1000
 
+enum {
+    SNAKE_UP,
+    SNAKE_DOWN,
+    SNAKE_LEFT,
+    SNAKE_RIGHT,
+};
+
+struct snake {
+    int x;
+    int y;
+    int dir;
+
+    struct snake *next;
+};
+
+typedef struct snake Snake;
+
+Snake *head;
+Snake *tail;
+
+void init_snake(){
+    Snake *new= malloc(sizeof(Snake));
+    new->x = rand() % (GRID_SIZE / 2) + (GRID_SIZE / 4);
+    new->y = rand() % (GRID_SIZE / 2) + (GRID_SIZE / 4);
+    new->dir = SNAKE_UP;
+    new->next = NULL;
+
+    head = new;
+    tail = new;
+
+    return;
+
+}
+
+void render_snake(SDL_Renderer *renderer, int x, int y){
+
+    SDL_SetRenderDrawColor(renderer, 0x00, 0xff, 0x00, 255);
+    int seg_size = GRID_DIM / GRID_SIZE - 10;
+    SDL_Rect seg;
+
+    seg.w = seg_size;
+    seg.h = seg_size;
+
+    Snake *track = head;
+
+    while (track != NULL){
+        seg.x + track->x * seg_size;
+        seg.y = y + track->y * seg_size;
+
+        SDL_RenderFillRect(renderer, &seg);
+
+        track = track->next;
+    }
+
+    return;
+}
+
+void increase_snake(){
+
+    Snake *new = malloc(sizeof(Snake));
+    new->x = tail->x;
+    new->y = tail->y - 1;
+    new->dir = tail->dir;
+
+    new->next = NULL;
+    tail->next = new;
+
+    tail = new;
+
+    return;
+
+}
+
+void move_snake(){
+    switch (head->dir) {
+        case SNAKE_UP:
+            head->y--;
+            break;
+        case SNAKE_DOWN:
+            head->y++;
+            break;
+        case SNAKE_LEFT:
+            head->x--;
+            break;
+        case SNAKE_RIGHT:
+            head->x++;
+            break;
+    }
+
+    return;
+}
+
 void render_grid(SDL_Renderer *renderer, int x, int y)
 {
     SDL_SetRenderDrawColor(renderer, 0x55, 0x55, 0X55, 255);
@@ -41,6 +133,9 @@ void render_grid(SDL_Renderer *renderer, int x, int y)
 
 
 int main(int argc, char* argv[]) {
+
+    init_snake();
+    increase_snake();
 
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -94,12 +189,18 @@ int main(int argc, char* argv[]) {
         SDL_RenderClear(renderer);
         //loop start
 
+        //move the snake
+        move_snake();
+
         render_grid(renderer, 300, 30);
+        render_snake(renderer, 300, 30);
 
         //loop end
         SDL_SetRenderDrawColor(renderer, 0x11, 0x11, 0x11, 255);
 
         SDL_RenderPresent(renderer);
+
+        SDL_Delay(200);
 
     }
 
